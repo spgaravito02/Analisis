d2b4f604bb3bd3e2dfd7c0d98b6c72e53be66439
Paola Garavito <spgaravito02@ucatolica.edu.co>
2012-08-04 13:08:40 -0500

    git pull

--- FIBONACCI.py
+++ FIBONACCI.py
@@ -1,16 +1,19 @@
 print "Solangie Paola Garavito 624702"
+print "Sucesion de Fibonacci:es la sucesion infinita de numeros naturales"
+print "esta inicia con 0 y 1, y a partir de ahi cada elemento es la suma"
+print "de los dos anteriores."
 print ""
 print "****************************************************"
 print "************* CALCULO SERIE FIBONACCI **************"
 print "****************************************************"
-valor = input("Ingrese el valor maximo que desea generear en la serie: ")
+valor = input("Ingrese el numero de la serie que desea generear: ")
 num1=0
 num2=1
 if valor>0:
     print "Los valores de la serie son:"
     print num1
     print num2
-    for i in range(0,valor):
+    for i in range(0,valor-2):
         num3=num1+num2
         num1=num2
         num2=num3
