94791e7bede0da4c03d9150d10b49673d682de30
Paola Garavito <spgaravito02@ucatolica.edu.co>
2012-08-10 17:17:15 -0500

    git pull

--- COMPARACION INSERCION Y MERGESORT.py
+++ COMPARACION INSERCION Y MERGESORT.py
@@ -4,19 +4,20 @@ print "****************************************************"
 print "*******ORDENAMIENTO POR INSERCION Y MERGSORT********"
 print "****************************************************"
 
-
-
 import random
 import time
 
 # Funcion para crear numeros aleatorios
 def  numeros(x):
     vec_ini=[]
+    vec_ini2=[]
     for i in range(0,x):
         dato=(random.choice (range(19))*random.choice(range(58)))/3
         vec_ini.append( [] )
         vec_ini[-1].append( dato )
-    return vec_ini
+        vec_ini2.append( [] )
+        vec_ini2[-1].append( dato )
+    return vec_ini,vec_ini2
 
 # Funcion para ordenar ascendentemente por el metodo de la Insercion
 def ordenar_insert(vector,x):
@@ -40,76 +41,59 @@ def ordenar_insert(vector,x):
 
 
 # Funcion para ordenar ascendentemente por el metodo de Mergsort
-
-def ordena_merg (arreglo1,n1,arreglo2,n2,arreglo3):
-    x1=0
-    x2=0
-    x3=0
-    while x1<n1 and x2<n2:
-        if arreglo1[x1]<arreglo2[x2]:
-            arreglo3[x3] = arreglo1[x1]
-            x1=x1+1
+def ordena_mergsort (arreglo1,n1,arreglo2,n2):
+    vect_ord=[]
+    i=0
+    j=0
+    while i < n1 and j < n2:
+        if  arreglo1[i]<=arreglo2[j]:
+            vect_ord.append(arreglo1[i])
+            i+=1
         else:
-            arreglo3[x3] = arreglo2[x2];
-            x2=x2+1
-            
-    while x1<n1:
-        arreglo3[x3] = arreglo1[x1]
-        x1=x1+1;
-        x3=x3+1;
-        
-    while x2<n2:
-        arreglo3[x3] = arreglo2[x2]
-        x2=x2;
-        x3=x3;
-    return arreglo3
-
-def ordenar_asc_merg(vector,x):
-    comp=0
-    inter=0
-    vec1=[]
-    vec2=[]
-    ini_relog= time.clock ()
+            vect_ord.append(arreglo2[j])
+            j+=1
+    vect_ord +=arreglo1[i:]
+    vect_ord +=arreglo2[j:]
+    
+def divide_mergsort(vector,x):
+    n1=0
+    n2=0
     if x>1:
-        if (x / 2)== 0:
-            n1=n2=(x/2)
+        print vector
+        if (x%2==0):
+            n1=n2= x / 2                     
         else:
-            n1=x/2
+            n1=x / 2
             n2=n1+1
-        for i in range(0,n1):
-            vec1[i]=vector[i]
-            i=i+1
-        for j in range(0,n2):
-            vec2[j]=vector[i];
-            i=i+1
-            j=j+1
-        ordenar_asc_merg(n1,vec1)
-        ordenar_asc_merg(n2,vec2)
-        ordena_merg(vec1, n1, vec2, n2, vec)
-    fin_relog=time.clock ()-ini_relog
-    print "Tiempo proceso:",fin_relog
-    return vector
+        mitad=len(vector)/2
+        print vector[:mitad]
+        print vector[mitad:]
+        arreglo1=divide_mergsort(vector[:mitad],n1)
+        print arreglo1
+        arreglo2=divide_mergsort(vector[mitad:],n2)
+        print arreglo2
+        return ordena_mergsort(arreglo1,n1,arreglo2,n2)                          
+
 
         
 
 #Arranque del programa
 cant=input("Ingrese la cantidad de numeros que desea ordenar: ")
-vec_des=numeros(cant)
+vec_merg,vec_insert = numeros(cant)
 print ""
-print "El vector a ordenar es:",vec_des
-
+print "El vector a ordenar es:",vec_insert
 #Imprime Metodo Insercion
 print ""
 print "-----------------------METODO INSERCION-----------------------"
-vec_ord=ordenar_insert(vec_des,cant)
+vec_ord=ordenar_insert(vec_insert,cant)
 print ""
-
+#Imprime Metodo Mersort
+print "-----------------------METODO MERGSORT------------------------"
+vec_ord=divide_mergsort(vec_merg,cant)
+print ""
+#print "Tiempo proceso:",fin_relog
+print ""
 print "El vector ordenado ascendentemente es:",vec_ord
-print ""
-print "-----------------------METODO MERGSORT-----------------------"
-vec_ord=ordenar_asc_merg(vec_des,cant)
-print ""
 
-print "El vector ordenado ascendentemente es:",vec_ord
 raw_input()
 
