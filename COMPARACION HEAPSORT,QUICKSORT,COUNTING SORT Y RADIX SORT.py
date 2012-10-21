c335da36d99ebfac33f44ac36cd23dc940dcc723
Paola Garavito <spgaravito02@ucatolica.edu.co>
2012-10-20 20:13:53 -0500

    git pull

--- COMPARACION HEAPSORT,QUICKSORT,COUNTING SORT Y RADIX SORT.py
+++ COMPARACION HEAPSORT,QUICKSORT,COUNTING SORT Y RADIX SORT.py
@@ -49,20 +49,31 @@ def crearheap(vector, inicio, fin):
             return
 
 # Funcion para ordenar ascendentemente por el metodo QUICKSORT
-def vec_quicksort(vector,tam):
-    inicio = (tam-2)/2
-    while inicio >= 0:
-        crearheap(vector, inicio, tam-1)
-        inicio=inicio-1
-    fin = tam-1    
-    while fin > 0:
-        vector[fin], vector[0] = vector[0], vector[fin]
-        crearheap(vector, 0, fin-1)
-        fin = fin-1
+def ordenar_quicksort(vector,izq,der):
+    i=izq
+    j=der
+    x=vector[(izq+der)/2]
+
+    while (i<=j):
+        while vector [i] < x and j<=der:
+            i=i+1
+        while x< vector [j] and j>izq:
+            j=j-1
+        if i<=j:
+            aux = vector [i]
+            vector [i]=vector [j]
+            vector [j]=aux
+            i=i+1
+            j=j-1
+        if izq < j:
+            ordenar_quicksort (vector,izq,j)
+    if i<der:
+        ordenar_quicksort (vector,i,der)
     return vector
-
-
+        
+        
     
+  
 
 
 
@@ -75,16 +86,18 @@ print ""
 print "El vector a ordenar es:",vec_insert
 #Imprime Metodo Heapsort
 print ""
-print "-----------------------METODO HEAPSORT-----------------------" 
+print "-----------------------METODO HEAPSORT-----------------------"
+print ""
 ini_relog= time.clock ()
 vec_ord=ordenar_heapsort(vec_heapsort,cant)
 fin_relog=time.clock ()-ini_relog
 print "Vector Ordenado:",vec_ord
 print "Tiempo proceso:",fin_relog
 print ""
-print "-----------------------METODO QUICKSORT-----------------------" 
+print "-----------------------METODO QUICKSORT-----------------------"
+print ""
 ini_relog= time.clock ()
-vec_ord=ordenar_quicksort(vec_quicksort,cant)
+vec_ord=ordenar_quicksort(vec_quicksort,0,cant-1)
 fin_relog=time.clock ()-ini_relog
 print "Vector Ordenado:",vec_ord
 print "Tiempo proceso:",fin_relog
