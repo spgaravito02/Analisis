818e67fe28e5be0954f5d61d7f7c83e2cdbf7800
Paola Garavito <spgaravito02@ucatolica.edu.co>
2012-10-20 22:39:11 -0500

    GIT PULL

--- COMPARACION HEAPSORT,QUICKSORT,COUNTING SORT Y RADIX SORT.py
+++ COMPARACION HEAPSORT,QUICKSORT,COUNTING SORT Y RADIX SORT.py
@@ -2,7 +2,7 @@ print "Solangie Paola Garavito 624702"
 print ""
 print "****************************************************"
 print "*******COMPARACION HEAPSORT,QUICKSORT,**************"
-print "*******  CCOUNTING SORT Y RADIX SORT  *************"
+print "*******  COUNTING SORT Y RADIX SORT  *************"
 print "****************************************************"
 
 import random
@@ -13,15 +13,14 @@ def  numeros(x):
     vec_ini=[]
     vec_ini2=[]
     vec_ini3=[]
+    vec_ini4=[]
     for i in range(0,x):
-        dato=(random.choice (range(19))*random.choice(range(58)))/3
-        vec_ini.append( [] )
-        vec_ini[-1].append( dato )
-        vec_ini2.append( [] )
-        vec_ini2[-1].append( dato )
-        vec_ini3.append( [] )
-        vec_ini3[-1].append( dato )
-    return vec_ini,vec_ini2,vec_ini3
+        dato=(random.choice (range(15))*random.choice(range(58)))/3
+        vec_ini.append(dato)
+        vec_ini2.append(dato)
+        vec_ini3.append(dato)
+        vec_ini4.append(dato)
+    return vec_ini,vec_ini2,vec_ini3,vec_ini4
 
 # Funcion para ordenar ascendentemente por el metodo HEAPSORT
 def ordenar_heapsort(vector,tam):
@@ -70,18 +69,28 @@ def ordenar_quicksort(vector,izq,der):
     if i<der:
         ordenar_quicksort (vector,i,der)
     return vector
-        
-        
+
+# Funcion para ordenar ascendentemente por el metodo COUNTING SORT
+def ordenar_counting(vector):
+    maximo= max(vector)
+    minimo= min(0, min(vector))
+    count_vector= [0]*(maximo-minimo+1)
+
+    for j in vector:
+        count_vector[j]+=count_vector[j]+1
+
+    vector_ordenado=[]
+    for i in range (minimo, maximo+1):
+        if count_vector[i]>0:
+            for k in range(0,count_vector[i]):
+                vector_ordenado.append(i)
+    return vector_ordenado
     
-  
 
-
-
-        
-
+ 
 #Arranque del programa
 cant=input("Ingrese la cantidad de numeros que desea ordenar: ")
-vec_heapsort,vec_quicksort,vec_insert = numeros(cant)
+vec_heapsort,vec_quicksort,vec_counting,vec_insert = numeros(cant)
 print ""
 print "El vector a ordenar es:",vec_insert
 #Imprime Metodo Heapsort
@@ -98,6 +107,14 @@ print "-----------------------METODO QUICKSORT-----------------------"
 print ""
 ini_relog= time.clock ()
 vec_ord=ordenar_quicksort(vec_quicksort,0,cant-1)
+fin_relog=time.clock ()-ini_relog
+print "Vector Ordenado:",vec_ord
+print "Tiempo proceso:",fin_relog
+print ""
+print "-----------------------METODO COUNTING SORT-----------------------"
+print ""
+ini_relog= time.clock ()
+vec_ord=ordenar_counting(vec_counting)
 fin_relog=time.clock ()-ini_relog
 print "Vector Ordenado:",vec_ord
 print "Tiempo proceso:",fin_relog
