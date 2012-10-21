d9391a1b32f2c619fecdfff76bcaa9a2efd779bb
Paola Garavito <spgaravito02@ucatolica.edu.co>
2012-10-21 00:08:48 -0500

    git pull

--- COMPARACION HEAPSORT,QUICKSORT,COUNTING SORT Y RADIX SORT.py
+++ COMPARACION HEAPSORT,QUICKSORT,COUNTING SORT Y RADIX SORT.py
@@ -7,6 +7,7 @@ print "****************************************************"
 
 import random
 import time
+import math
 
 # Funcion para crear numeros aleatorios
 def  numeros(x):
@@ -14,13 +15,15 @@ def  numeros(x):
     vec_ini2=[]
     vec_ini3=[]
     vec_ini4=[]
+    vec_ini5=[]
     for i in range(0,x):
-        dato=(random.choice (range(15))*random.choice(range(58)))/3
+        dato=((random.choice (range(15))*random.choice(range(19)))/3)+1
         vec_ini.append(dato)
         vec_ini2.append(dato)
         vec_ini3.append(dato)
         vec_ini4.append(dato)
-    return vec_ini,vec_ini2,vec_ini3,vec_ini4
+        vec_ini5.append(dato)
+    return vec_ini,vec_ini2,vec_ini3,vec_ini4,vec_ini5
 
 # Funcion para ordenar ascendentemente por el metodo HEAPSORT
 def ordenar_heapsort(vector,tam):
@@ -45,7 +48,7 @@ def crearheap(vector, inicio, fin):
             vector[i], vector[j] = vector[j], vector[i]
             i = j
         else:
-            return
+            return vector
 
 # Funcion para ordenar ascendentemente por el metodo QUICKSORT
 def ordenar_quicksort(vector,izq,der):
@@ -75,9 +78,8 @@ def ordenar_counting(vector):
     maximo= max(vector)
     minimo= min(0, min(vector))
     count_vector= [0]*(maximo-minimo+1)
-
     for j in vector:
-        count_vector[j]+=count_vector[j]+1
+        count_vector[j]+=1
 
     vector_ordenado=[]
     for i in range (minimo, maximo+1):
@@ -86,11 +88,26 @@ def ordenar_counting(vector):
                 vector_ordenado.append(i)
     return vector_ordenado
     
-
+# Funcion para ordenar ascendentemente por el metodo RADIX SORT
+def ordenar_radix(vector):
+    maxLen = -1
+    for number in vector: 
+        numLen = int(math.log10(number)) + 1
+        if numLen > maxLen:
+            maxLen = numLen
+    buckets = [[] for i in range(0, 10)] 
+    for digit in range(0, maxLen):
+        for number in vector:
+            buckets[number / 10**digit % 10].append(number)
+        del vector[:]
+        for bucket in buckets:
+            vector.extend(bucket)
+            del bucket[:]
+    return vector
  
 #Arranque del programa
 cant=input("Ingrese la cantidad de numeros que desea ordenar: ")
-vec_heapsort,vec_quicksort,vec_counting,vec_insert = numeros(cant)
+vec_heapsort,vec_quicksort,vec_counting,vec_radix,vec_insert = numeros(cant)
 print ""
 print "El vector a ordenar es:",vec_insert
 #Imprime Metodo Heapsort
@@ -119,7 +136,14 @@ fin_relog=time.clock ()-ini_relog
 print "Vector Ordenado:",vec_ord
 print "Tiempo proceso:",fin_relog
 print ""
-
+print "-----------------------METODO RADIX SORT-----------------------"
+print ""
+ini_relog= time.clock ()
+vec_ord=ordenar_radix(vec_radix)
+fin_relog=time.clock ()-ini_relog
+print "Vector Ordenado:",vec_ord
+print "Tiempo proceso:",fin_relog
+print ""
 
 raw_input()
 
